7f3316a8c3d26a110bb20881c7d0b172a49dc086
Ade <tik.eskanor@gmail.com>
2024-08-17 15:58:32 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import data from "./data"
 
-function App() {
+export default function App()
+{
+  let [input,setInput] = React.useState(0)
+  let [texts,setTexts] = React.useState([])
+
+  function handleSubmit(e)
+  {
+     e.preventDefault();
+
+     let amount = parseInt(input)
+     
+     if(amount <= 0)
+     {
+       amount = 1
+     }
+     if(amount > 8)
+     {
+       amount = 8
+     }
+
+     setTexts(data.slice(0,amount))
+     console.log(texts)
+  }
+
+  function handleChange(e)
+  {
+     let value = e.target.value
+     setInput(value)
+  }
+
+  let paragraphs = texts.map((item, index)=>{
+    return (
+      <p key={index}>{item}</p>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="container">
+      <h2>Tired of boring old lorem ipum</h2>
+        
+        <form onSubmit={handleSubmit}>
+            <p>Paragraphs</p>
+            <div>
+            <input type="number" onChange={handleChange} value={input}/>
+            <button>Generate</button>
+            </div>
+           
+        </form>
+       <div className="paragraphs">
+          {paragraphs}
+       </div>
+      </div>
+    </main>
+  )
 }
-
-export default App;
